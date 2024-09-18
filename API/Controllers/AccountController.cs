7245293542959fd95d8fe3d55e7feb0216fdef41
kikin787 <141957541+kikin787@users.noreply.github.com>
2024-09-18 07:58:25 -0600

    Parcial - Add LoginRequest

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,31 +1,45 @@
-
-
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
-    [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> RegisterAsync(
-        [FromBody] string username, 
-        [FromBody] string password)
+    [HttpPost("registrer")]
+    public async Task<ActionResult<AppUser>> RegistrerAsync(RegisterDto request)
     {
-        using var hmac = new HMACSHA512();
+        if (await UserExistsAsync(request.UserName))
+        {
+            return BadRequest("Username already in use");
+        }
 
+        using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = request.UserName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
             PasswordSalt = hmac.Key
         };
+
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
         return user;
     }
+
+    // [HttpPost("login")]
+    // public async Task<ActionResult<AppUser>> Login(LoginRequest request)
+    // {
+
+    // }
+
+    private async Task<bool> UserExistsAsync(string username) =>
+    await context.Users.AnyAsync(
+          user => user.UserName.ToLower() == username.ToLower());
+
 }
