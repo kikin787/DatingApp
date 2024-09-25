537dc05cb3456babddf5f6334f55a1cb89f42bd0
velez <techie13@innovatechminds.com>
2024-09-24 20:12:04 -0600

    Parcial 01 - Add Nav & Add Format HTML to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NavComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrección: styleUrls en plural
 })
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+    this.http.get("https://localhost:5001/api/users").subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log("Request completed")
