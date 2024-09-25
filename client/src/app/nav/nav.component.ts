bf183397c9513ee75b5abbc35d5d920616537988
velez <techie13@innovatechminds.com>
2024-09-25 07:44:54 -0600

    Parcial 01 - Injected account service into component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_service/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,10 +10,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService)
+  loggedIn = false;
   model: any = {};
 
   login(): void {
-    console.log(this.model)
-  }
+  this.accountService.login(this.model).subscribe({
+    next: (response) => {
+      console.log(response);
+      this.loggedIn = true;
+    },
+    error: (error) => {
+      console.log(error)
+    }
+  })  
+}
 
 }
