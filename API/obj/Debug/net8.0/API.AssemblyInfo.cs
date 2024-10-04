1020526fa588816ad8c4edc1c18eba2ae4a2d68d
velez <techie13@innovatechminds.com>
2024-10-03 21:08:04 -0600

    Update API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7f242ab47c8e2d005230b0561b241dd2d671a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb43e72eab52ab2bde79804cbea0ca3bd60d1b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
