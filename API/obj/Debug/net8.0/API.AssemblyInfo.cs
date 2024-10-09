44e36616409b89cb8d8909ab4b28b4f0b6c53c06
velez <techie13@innovatechminds.com>
2024-10-09 16:48:27 -0600

    Parcial02 - Add Bootswatch theme

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f140f94962604720a849c652b7f7f51438baf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b5565d1c5db27a07914337045c38fa83915b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
