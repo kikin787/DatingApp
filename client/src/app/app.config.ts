b6a156bb0c6f810dcf9cce4556fe0d6ed80f6ce5
velez <techie13@innovatechminds.com>
2024-10-08 22:52:12 -0600

    Parcial02 - Add notification service

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import * as ngxToastr from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    ngxToastr.provideToastr({
+      positionClass: "toast-bottom-right"
+    })
   ]
 };
