08350e28f152aa0d6eddc3782d9fee950e9cc43d
velez <techie13@innovatechminds.com>
2024-10-14 09:46:52 -0600

    Parcial02 - Add Exception MIDDLEWARE

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,15 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-app.UseCors((cors) => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors((cors) => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
