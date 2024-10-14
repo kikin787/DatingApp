2143167e04eee6e9b1092b5e0fd98684ec24ff80
velez <techie13@innovatechminds.com>
2024-10-14 09:55:09 -0600

    Parcial02 - Add tests for errors from SPA

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,20 +4,22 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { authGuard as authGuard_1 } from './_guards/auth.guard';
+import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     {
         path: "",
         runGuardsAndResolvers: "always",
-        canActivate: [authGuard_1],
+        canActivate: [authGuard],
         children: [
-            { path: "members", component: MemberListComponent, canActivate: [authGuard_1] },
+            { path: "members", component: MemberListComponent, canActivate: [authGuard] },
             { path: "members/:id", component: MemberDetailComponent },
             { path: "lists", component: ListsComponent },
             { path: "messages", component: MessagesComponent },
         ]
     },
+    { path: "errors", component: TestErrorsComponent },
     { path: "**", component: HomeComponent, pathMatch: "full" },
 ];
