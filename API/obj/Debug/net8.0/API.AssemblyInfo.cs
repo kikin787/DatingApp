175f3a033cd134f4a3a4008958d67887a32ad724
velez <techie13@innovatechminds.com>
2024-10-24 14:12:53 -0600

    Parcial02 - Add REPOSITORY PATTERN

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697d419d8ee485fe0bb9bf7e72d01bf5e08f23e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb67a01789047145b0f50461b39e530b5d65476")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
