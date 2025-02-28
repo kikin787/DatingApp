187680cc56ac4a55f8cc283da11357e1fda50bd4
kikin787 <141957541+kikin787@users.noreply.github.com>
2025-02-28 13:32:40 -0600

    Parcial04 - Add PUT for photos

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,7 +8,6 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 [Authorize]
-
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
