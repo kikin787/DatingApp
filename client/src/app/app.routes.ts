80e227d76a9a8530ae592c2b85f59bf6d93283df
kikin787 <141957541+kikin787@users.noreply.github.com>
2025-05-08 13:57:56 -0600

    Parcial02 - Add Route resolver

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,7 +11,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
-
+// 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {
