404475f6bb66d6746bbcc7367c6532dde0516e9e
kikin787 <141957541+kikin787@users.noreply.github.com>
2025-06-19 22:55:04 -0600

    Parcial06 - Add send message to Message Hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
 public class Startup
 {
     private readonly IConfiguration _config;
